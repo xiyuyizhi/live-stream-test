a6b71f5023911f9860213324b39b6c75bfb6f1f4
xiyuyizhi <1553810509@qq.com>
2019-08-31 00:19:12 +0800

    add wss

--- config.js
+++ config.js
@@ -4,13 +4,15 @@ if (process.env.NODE_ENV === 'production') {
   ffmpegPath = '/root/ffmpeg/ffmpeg'
   streamAddress = {
     ts: 'http://live.xiyuyizhi.xyz/live/test/index.m3u8',
-    flv: 'http://live.xiyuyizhi.xyz/live/test.flv'
+    flv: 'http://live.xiyuyizhi.xyz/live/test.flv',
+    wss: 'ws://live.xiyuyizhi.xyz/live/test.flv'
   }
 } else {
   ffmpegPath = '/usr/local/bin/ffmpeg';
   streamAddress = {
     ts: 'http://localhost:7990/live/test/index.m3u8',
-    flv: 'http://localhost:7990/live/test.flv'
+    flv: 'http://localhost:7990/live/test.flv',
+    wss: 'ws://localhost:7990/live/test.flv'
   }
 }
 
